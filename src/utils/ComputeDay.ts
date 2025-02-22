000a2dc87f0bbd0282c14dc7b0d83742b4911190
孙奥 <115922135+Su1furicAcid@users.noreply.github.com>
2025-02-22 18:22:21 +0800

    chore: update ComputeDay.ts

--- src/utils/ComputeDay.ts
+++ src/utils/ComputeDay.ts
@@ -1,5 +1,29 @@
 /**
- * 根据出生日期和今天日期计算下一个生日的日期
+ * @abstract 获取今天的日期
+ * @returns {string} today e.g. '2021-01-01'
+ */
+
+// TODO: 请在此处实现函数
+
+/**
+ * @abstract 计算某个日期前 n 天的日期
+ * @param {string} date e.g. '2021-01-01'
+ * @param {number} days e.g. 1
+ * @returns {string} date e.g. '2020-12-31'
+ */
+
+// TODO: 请在此处实现函数
+
+/**
+ * @abstract 计算某个日期最相近的周六的日期
+ * @param {string} date e.g. '2021-01-01'
+ * @returns {string} date e.g. '2021-01-02'
+ */
+
+// TODO: 请在此处实现函数
+
+/**
+ * @abstract 根据出生日期和今天日期计算下一个生日的日期
  * @param {string} birthday e.g. '1999-01-01'
  * @param {string} today e.g. '2021-01-01'
  * @returns {string} nextBirthday e.g. '2022-01-01'
@@ -19,7 +43,7 @@ const getNextBirthday = (birthday: string, today: string): string => {
 };
 
 /**
- * 根据下一个生日日期和今天日期计算距离下一个生日的天数
+ * @abstract 根据下一个生日日期和今天日期计算距离下一个生日的天数
  * @param {string} nextBirthday e.g. '2022-01-01'
  * @param {string} today e.g. '2021-01-01'
  * @returns {number} days e.g. 365
@@ -31,4 +55,4 @@ const getDaysToNextBirthday = (nextBirthday: string, today: string): number => {
     return Math.floor((nextBirthdayTimestamp - todayTimestamp) / (1000 * 60 * 60 * 24));
 };
 
-export { getNextBirthday, getDaysToNextBirthday };
+export { getNextBirthday, getDaysToNextBirthday }; // TODO: 添加已经实现的函数
